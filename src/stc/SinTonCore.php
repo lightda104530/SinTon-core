bde482a2fff2515229af447107902aebcaaed882
TSRlightda <lightda104530@gmail.com>
2017-03-10 21:47:19 +0800

    Oh yeah!

--- src/stc/SinTonCore.php
+++ src/stc/SinTonCore.php
@@ -1,11 +1,113 @@
 <?php
 
 namespace SinTonCore;
+/* Oh my...let's read some things in pocketmine documents!
 
-use pocketmine\Server;
+/* Reading Player */
+use pocketmine\Player;
+/* Reading Inventorys */
+use pocketmine\inventory\Inventory;
+use pocketmine\inventory\ChestInventory;
+use pocketmine\inventory\DoubleChestInventory;
+/* Reading Events */
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerBedEnterEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerHungerChangeEvent;
+/* Reading Items */
+use pocketmine\item\Slimeball;
+use pocketmine\item\Item;
+/* Reading Commands */
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\command\ConsoleCommandSender;
+/* Reading Utils */
 use pocketmine\utils\TextFormat;
+use pocketmine\utils\config;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\BinaryStream;
+use pocketmine\utils\Binary;
+/* Reading Entitys */
+use pocketmine\entity\Entity;
+use pocketmine\entity\Effect;
+use pocketmine\entity\Arrow;
+/* Reading Levels */
+use pocketmine\level\sound\BlazeShootSound;
+use pocketmine\level\sound\GhastShootSound;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\level\sound\AnvilFallSound;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\level\particle\ExplodeParticle;
+use pocketmine\level\particle\BubbleParticle;
+use pocketmine\level\particle\Particle;
+use pocketmine\level\particle\DustParticle;
+use pocketmine\level\Position;
+use pocketmine\level\Location;
+use pocketmine\level\Position\getLevel;
+use pocketmine\level\format\Chunk;
+use pocketmine\level\format\FullChunk;
+use pocketmine\level\Level;
+/* Reading Blocks */
+use pocketmine\block\IronOre;
+use pocketmine\block\GoldOre;
+use pocketmine\block\Block;
+/* Reading Plugins */
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\listener;
+use pocketmine\plugin\PluginManager;
+use pocketmine\plugin\Plugin;
+/* Reading Server */
+use pocketmine\Server;
+/* Reading Network */
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\PlayerActionPacket;
+use pocketmine\network\protocol\MovePlayerPacket;
+use pocketmine\network\protocol\BlockEventPacket;
+/* Reading Math */
+use pocketmine\math\Vector3;
+use pocketmine\math\Math;
+use pocketmine\math\AxisAlignedBB;
+/* Reading Schedulers */
+use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\CallbackTask;
+/* Reading Nbts */
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\CompoundTag;
+/* Reading Permission */
+use pocketmine\permission\Permission;
+/* Now let's start the core */
+
 
 class SinTonCore extends PluginBase implements listener{
 
